57d45bd0bb222242990bbdd76044b6d21eb7ed2a
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2012-12-14 17:03:28 -0200

    Initial commit.

---
+++ PopulationClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopulationClock
+//
+//  Created by Fernando Lemos on 14/12/12.
+//  Copyright (c) 2012 NetFilter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopulationClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PopulationClock
+//
+//  Created by Fernando Lemos on 14/12/12.
+//  Copyright (c) 2012 NetFilter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
