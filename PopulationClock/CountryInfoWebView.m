abb3a22fa50224e26b64585bba188e61cf2a255e
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-01-04 11:59:45 -0200

    Added crude birth and death rate to the stats.

--- PopulationClock/CountryInfoWebView.m
+++ PopulationClock/CountryInfoWebView.m
@@ -180,6 +180,8 @@ static NSString * const StatFormat = @"<div class=\"metric\"><span class=\"key\"
         ADD_STAT(@"gdpGrowth", NSLocalizedString(@"GDP growth", @""), formatPercentage);
         ADD_STAT(@"lifeExpectancy", NSLocalizedString(@"Life expectancy", @""), formatYears);
         ADD_STAT(@"birthsPerWoman", NSLocalizedString(@"Births per woman", @""), formatSmallNumber);
+        ADD_STAT(@"birthRate", NSLocalizedString(@"Crude birth rate per 1,000 people", @""), formatSmallNumber);
+        ADD_STAT(@"deathRate", NSLocalizedString(@"Crude death rate per 1,000 people", @""), formatSmallNumber);
         ADD_STAT(@"healthExpensePercentGDP", NSLocalizedString(@"Health expenditure % GDP", @""), formatPercentage);
         ADD_STAT(@"literacyRate", NSLocalizedString(@"Literacy rate", @""), formatPercentage);
         ADD_STAT(@"govtEducationExpensePercentGDP", NSLocalizedString(@"Govt. education expenditure % GDP", @""), formatPercentage);
