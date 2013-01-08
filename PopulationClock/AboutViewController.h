90f4fbf53acae5319be1603ce4ca9a661bb0a2c1
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-01-08 11:34:10 -0200

    Basic functionality for the about screen.

--- PopulationClock/AboutViewController.h
+++ PopulationClock/AboutViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2012 NetFilter. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface AboutViewController : UIViewController
 
 @end
--- PopulationClock/AboutViewController.m
+++ PopulationClock/AboutViewController.m
@@ -6,33 +6,37 @@
 //  Copyright (c) 2012 NetFilter. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "AboutViewController.h"
 
-@interface AboutViewController ()
-
-@end
-
-@implementation AboutViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+@implementation AboutViewController {
+    IBOutlet __weak UINavigationBar *_navigationBar;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+- (void)viewDidLoad {
+    // Set text here so we don't have to translate the storyboard
+    _navigationBar.topItem.title = NSLocalizedString(@"About Population Clock", @"");
+    
+    // Set the navigation bar theme
+    NSDictionary *attrs = @{UITextAttributeTextColor : [UIColor colorWithRed:0xfa/255.0 green:0xc4/255.0 blue:0x2a/255.0 alpha:1]};
+    [[UINavigationBar appearance] setTitleTextAttributes:attrs];
+    
+    // Set the background color
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"debut_light"]];
+    
+    // Add a gradient to the background
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.view.bounds;
+    gradient.colors = @[
+        (id)[UIColor colorWithRed:1 green:1 blue:1 alpha:0.5].CGColor,
+        (id)[UIColor colorWithRed:0 green:0 blue:0 alpha:0.5].CGColor
+    ];
+    [self.view.layer insertSublayer:gradient atIndex:0];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)doneButtonTouched:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
