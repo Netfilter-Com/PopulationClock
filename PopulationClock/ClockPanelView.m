9920c3e802b968eff75114da8cdd8a0b10f3c66a
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-01-08 10:32:37 -0200

    Use NSAttributeStrings instead.
    Allows us to make the label bigger and resize automatically if
    there's not enough space, as it's a single space. In iOS 5, we just
    use regular strings, so we get a boring, single font style label.

--- PopulationClock/ClockPanelView.m
+++ PopulationClock/ClockPanelView.m
@@ -11,15 +11,11 @@
 
 @implementation ClockPanelView {
     IBOutlet __weak UIImageView *_backgroundImageView;
-    IBOutlet __weak UILabel *_titleLabel;
     IBOutlet __weak UILabel *_countryNameLabel;
     IBOutlet __weak UIView *_clock;
 }
 
 - (void)awakeFromNib {
-    // Set the text of the title label
-    _titleLabel.text = NSLocalizedString(@"POPULATION:", @"");
-    
     // Observe changes to the country selection
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(countrySelectionChanged:) name:CountrySelectionNotification object:nil];
 }
@@ -33,9 +29,25 @@
     // Get the selection
     NSString *selection = notification.userInfo[SelectedCountryKey];
     
-    // Update the country name label
+    // Get the text we'll use in the label
     NSDictionary *info = [DataManager sharedDataManager].countryData[selection];
-    _countryNameLabel.text = [info[@"name"] uppercaseString];
+    NSString *populationText = NSLocalizedString(@"POPULATION: ", @"");
+    NSString *countryNameText = [info[@"name"] uppercaseString];
+    
+    // Create the attributed string and update the label
+    if (&NSFontAttributeName == NULL) {
+        _countryNameLabel.text = [populationText stringByAppendingString:countryNameText];
+    }
+    else {
+        NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:populationText attributes:@{
+            NSFontAttributeName : [UIFont systemFontOfSize:16]
+        }];
+        NSAttributedString *nameAttr = [[NSAttributedString alloc] initWithString:countryNameText attributes:@{
+            NSFontAttributeName : [UIFont boldSystemFontOfSize:16]
+        }];
+        [str appendAttributedString:nameAttr];
+         _countryNameLabel.attributedText = str;
+    }
     
     // We'll need to layout the subviews again to align the labels
     [self setNeedsLayout];
@@ -53,36 +65,22 @@
     else
         _backgroundImageView.image = [UIImage imageNamed:@"bgClockVert"];
     
-    // The labels use different sizes depending on the orientation
-    CGFloat fontSize = UIInterfaceOrientationIsLandscape(orientation) ? 11 : 13;
-    _titleLabel.font = [UIFont systemFontOfSize:fontSize];
-    _countryNameLabel.font = [UIFont boldSystemFontOfSize:fontSize];
-    [_titleLabel sizeToFit];
-    [_countryNameLabel sizeToFit];
-    
     // Determine the total height of the elements we'll center and
-    // derive the Y origin of the labels from that
-    CGFloat height = MAX(_titleLabel.frame.size.height, _countryNameLabel.frame.size.height) + 8 + _clock.frame.size.height;
+    // derive the Y origin of the label from that
+    [_countryNameLabel sizeToFit];
+    CGFloat height = _countryNameLabel.frame.size.height + 8 + _clock.frame.size.height;
     CGFloat originY = (self.bounds.size.height - height) / 2;
     
-    // Determine the total width of the labels and derive
-    // the X origin of the first label from that
-    CGFloat labelsWidth = _titleLabel.frame.size.width + 4 + _countryNameLabel.frame.size.width;
-    CGFloat originX = (self.bounds.size.width - labelsWidth) / 2;
-    
-    // We can now position the first label
-    CGRect frame = _titleLabel.frame;
-    frame.origin.x = originX;
-    frame.origin.y = originY;
-    _titleLabel.frame = frame;
-    
-    // And the second label
-    frame = _countryNameLabel.frame;
-    frame.origin.x = originX + _titleLabel.frame.size.width + 4;
+    // We can now position the label
+    CGRect frame = _countryNameLabel.frame;
+    CGFloat maxWidth = self.bounds.size.width - 40;
+    if (frame.size.width > maxWidth)
+        frame.size.width = maxWidth;
+    frame.origin.x = (self.bounds.size.width - frame.size.width) / 2;
     frame.origin.y = originY;
     _countryNameLabel.frame = frame;
     
-    // And finally the clock
+    // And then the clock
     frame = _clock.frame;
     frame.origin.x = (self.bounds.size.width - frame.size.width) / 2;
     frame.origin.y = originY + height - frame.size.height;
