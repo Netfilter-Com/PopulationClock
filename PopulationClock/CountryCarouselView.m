e060dc3997d2c4db7773a5c2224a85f4afd9d70b
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-02-14 17:42:29 -0200

    Abstract the country carousel so we can reuse it

---
+++ PopulationClock/CountryCarouselView.m
@@ -0,0 +1,204 @@
+//
+//  CountryCarouselView.m
+//  PopulationClock
+//
+//  Created by Fernando Lemos on 14/02/13.
+//  Copyright (c) 2013 NetFilter. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+#import "DataManager.h"
+#import "CountryCarouselView.h"
+#import "UIImage+NFResizable.h"
+
+@interface CountryCarouselView ()
+
+@property (nonatomic, weak) IBOutlet UIButton *leftArrow;
+@property (nonatomic, weak) IBOutlet UIButton *rightArrow;
+
+@end
+
+@implementation CountryCarouselView {
+    UIImageView *_flags[3];
+    NSString *_countryCodes[3];
+}
+
+- (void)dealloc
+{
+    // We are no longer observers
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)awakeFromNib
+{
+    // We are our own delegate
+    self.delegate = self;
+    
+    // Observe changes to the country selection
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(countrySelectionChanged:) name:CountrySelectionNotification object:nil];
+}
+
+- (void)countrySelectionChanged:(NSNotification *)notification
+{
+    // Nothing to do if we are the source of the notification
+    if (notification.object == self) {
+        return;
+    }
+    
+    // Get the selection and reload the flags
+    NSString *selection = notification.userInfo[SelectedCountryKey];
+    [self reloadWithCountry:selection];
+}
+
+- (void)reloadWithCountry:(NSString *)countryCode
+{
+    // Find the index of the selection
+    NSArray *countries = [DataManager sharedDataManager].orderedCountryData;
+    NSUInteger index = NSNotFound;
+    for (int i = 0; i < countries.count; ++i) {
+        NSDictionary *info = countries[i];
+        if ([info[@"code"] isEqualToString:countryCode]) {
+            index = i;
+            break;
+        }
+    }
+    assert(index != NSNotFound);
+    
+    // Remove all subviews
+    for (UIView *subview in self.subviews) {
+        [subview removeFromSuperview];
+    }
+    
+    /*
+     * Note that the way we implemented borders really sucks. The reason we're doing
+     * this is that CALayer borders overlap the content. I couldn't get alternatives
+     * like drawing a new image with the borders to work as they all got ridiculously
+     * blurry. This should be possible, though, so if you can figure out, please
+     * replace this crap with something that doesn't suck as much.
+     */
+    
+    // Change the portrait flags
+    int indices[3] = { index == 0 ? countries.count - 1 : index - 1, index, (index + 1) % countries.count };
+    for (int i = 0; i < 3; ++i) {
+        // Get the country info and country code
+        NSDictionary *info = countries[indices[i]];
+        NSString *countryCode = info[@"code"];
+        
+        // Get the flag into an image view and apply a border and shadow
+        // unless we're showing the globe
+        UIImageView *flag;
+        if ([countryCode isEqualToString:@"world"]) {
+            flag = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"globeVertical"]];
+        } else {
+            // Get the right flag
+            NSString *flagName = [NSString stringWithFormat:@"country_flag_%@", countryCode];
+            UIImage *image = [UIImage imageNamed:flagName];
+            
+            // Calculate its new size, including borders
+            CGFloat scale = 126 / image.size.height;
+            CGSize newSize = CGSizeMake(floorf(image.size.width * scale), floorf(image.size.height * scale));
+            
+            // Calculate the size of the image centered in the
+            // image view by insetting the border width
+            CGSize innerSize = newSize;
+            innerSize.width -= 8;
+            innerSize.height -= 8;
+            
+            // Create the image view and configure it
+            flag = [[UIImageView alloc] initWithImage:[image nf_resizedImageWithSize:innerSize]];
+            flag.frame = CGRectMake(0, 0, newSize.width, newSize.height);
+            flag.contentMode = UIViewContentModeCenter;
+            flag.backgroundColor = [UIColor whiteColor];
+            
+            // Add the shadow effect
+            flag.layer.shadowOffset = CGSizeMake(2, 2);
+            flag.layer.shadowColor = [UIColor blackColor].CGColor;
+            flag.layer.shadowOpacity = 0.6;
+            flag.layer.shouldRasterize = YES;
+            flag.layer.rasterizationScale = [UIScreen mainScreen].scale;
+        }
+        
+        // Save the flag and country code
+        _flags[i] = flag;
+        _countryCodes[i] = countryCode;
+        
+        // Add the flag
+        [self addSubview:flag];
+    }
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    // Give precedence to the arrows if we're not animating
+    // and the middle flag is the one that is currently shown
+    // (in case we haven't swapped the flags yet)
+    if (!self.layer.animationKeys.count && self.contentOffset.x == self.frame.size.width) {
+        UIView *hit = [self.leftArrow hitTest:[self.leftArrow convertPoint:point fromView:self] withEvent:event];
+        if (hit) {
+            return hit;
+        }
+        hit = [self.rightArrow hitTest:[self.rightArrow convertPoint:point fromView:self] withEvent:event];
+        if (hit) {
+            return hit;
+        }
+    }
+    
+    // Operate as normal
+    return [super hitTest:point withEvent:event];
+}
+
+- (void)layoutSubviews
+{
+    for (int i = 0; i < 3; ++i) {
+        UIImageView *flag = _flags[i];
+        flag.center = CGPointMake(self.bounds.size.width * (i + 0.5), self.frame.size.height / 2);
+    }
+}
+
+- (IBAction)leftArrowTouched:(id)sender
+{
+    [self setContentOffset:CGPointMake(0, 0) animated:YES];
+}
+
+- (IBAction)rightArrowTouched:(id)sender
+{
+    [self setContentOffset:CGPointMake(self.frame.size.width * 2, 0) animated:YES];
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    // This is called after the animation triggered
+    // by setting the content offset programatically
+    [self checkSelectedCountry];
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    // This is called after the decelerating animation
+    // when the user releases the dragged scrollview
+    [self checkSelectedCountry];
+}
+
+- (void)checkSelectedCountry
+{
+    // Find the selected page
+    int page = self.contentOffset.x / self.frame.size.width;
+    assert(page == 0 || page == 1 || page == 2);
+    
+    // If it's the middle page, the selection hasn't changed
+    if (page == 1) {
+        return;
+    }
+    
+    // Reload the flags
+    NSString *selection = _countryCodes[page];
+    [self reloadWithCountry:selection];
+    
+    // Let others know about this selection
+    [[NSNotificationCenter defaultCenter] postNotificationName:CountrySelectionNotification
+                                                        object:self
+                                                      userInfo:@{SelectedCountryKey : selection}];
+}
+
+@end
--- PopulationClock/CountryInfoViewController.m
+++ PopulationClock/CountryInfoViewController.m
@@ -6,8 +6,6 @@
 //  Copyright (c) 2012 NetFilter. All rights reserved.
 //
 
-#import <QuartzCore/QuartzCore.h>
-
 #import "CountryInfoViewController.h"
 #import "DataManager.h"
 #import "UIImage+NFResizable.h"
@@ -15,14 +13,12 @@
 @implementation CountryInfoViewController {
     IBOutlet __weak UIImageView *_backgroundImageView;
     IBOutlet __weak UIScrollView *_portraitScrollView;
-    IBOutlet __weak UIImageView *_portraitArrowLeft;
-    IBOutlet __weak UIImageView *_portraitArrowRight;
+    IBOutlet __weak UIButton *_portraitArrowLeft;
+    IBOutlet __weak UIButton *_portraitArrowRight;
     IBOutlet __weak UIView *_portraitWebViewBackground;
     IBOutlet __weak UIImageView *_landscapeFlag;
     IBOutlet __weak UILabel *_landscapeCountryName;
     IBOutlet __weak UIWebView *_webView;
-    UIImageView *_portraitFlags[3];
-    NSString *_portraitCountryCodes[3];
 }
 
 - (void)dealloc {
@@ -31,97 +27,16 @@
 }
 
 - (void)viewDidLoad {
-    // Set up the gesture recognizer for the arrows
-    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewTouched:)];
-    recognizer.numberOfTapsRequired = 1;
-    [_portraitScrollView addGestureRecognizer:recognizer];
-    
     // Observe changes to the country selection
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(countrySelectionChanged:) name:CountrySelectionNotification object:nil];
 }
 
 - (void)countrySelectionChanged:(NSNotification *)notification {
-    // Note that we don't ignore notifications coming from ourselves
-    // because even in that case we still have to change the flags
-    // and other info.
-    
     // Get the selection
     NSString *selection = notification.userInfo[SelectedCountryKey];
     
-    // Find the index of the selection
-    NSArray *countries = [DataManager sharedDataManager].orderedCountryData;
-    NSUInteger index = NSNotFound;
-    for (int i = 0; i < countries.count; ++i) {
-        NSDictionary *info = countries[i];
-        if ([info[@"code"] isEqualToString:selection]) {
-            index = i;
-            break;
-        }
-    }
-    assert(index != NSNotFound);
-    
-    // Remove all of the scrollview's subviews
-    for (UIView *subview in _portraitScrollView.subviews)
-        [subview removeFromSuperview];
-    
-    /*
-     * Note that the way we implemented borders really sucks. The reason we're doing
-     * this is that CALayer borders overlap the content. I couldn't get alternatives
-     * like drawing a new image with the borders to work as they all got ridiculously
-     * blurry. This should be possible, though, so if you can figure out, please
-     * replace this crap with something that doesn't suck as much.
-     */
-    
-    // Change the portrait flags
-    int indices[3] = { index == 0 ? countries.count - 1 : index - 1, index, (index + 1) % countries.count };
-    for (int i = 0; i < 3; ++i) {
-        // Get the country info and country code
-        NSDictionary *info = countries[indices[i]];
-        NSString *countryCode = info[@"code"];
-        
-        // Get the flag into an image view and apply a border and shadow
-        // unless we're showing the globe
-        UIImageView *flag;
-        if ([countryCode isEqualToString:@"world"]) {
-            flag = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"globeVertical"]];
-        }
-        else {
-            // Get the right flag
-            NSString *flagName = [NSString stringWithFormat:@"country_flag_%@", countryCode];
-            UIImage *image = [UIImage imageNamed:flagName];
-            
-            // Calculate its new size, including borders
-            CGFloat scale = 126 / image.size.height;
-            CGSize newSize = CGSizeMake(floorf(image.size.width * scale), floorf(image.size.height * scale));
-            
-            // Calculate the size of the image centered in the
-            // image view by insetting the border width
-            CGSize innerSize = newSize;
-            innerSize.width -= 8;
-            innerSize.height -= 8;
-            
-            // Create the image view and configure it
-            flag = [[UIImageView alloc] initWithImage:[image nf_resizedImageWithSize:innerSize]];
-            flag.frame = CGRectMake(0, 0, newSize.width, newSize.height);
-            flag.contentMode = UIViewContentModeCenter;
-            flag.backgroundColor = [UIColor whiteColor];
-            
-            // Add the shadow effect
-            flag.layer.shadowOffset = CGSizeMake(2, 2);
-            flag.layer.shadowColor = [UIColor blackColor].CGColor;
-            flag.layer.shadowOpacity = 0.6;
-        }
-        
-        // Save the flag and country ocde
-        _portraitFlags[i] = flag;
-        _portraitCountryCodes[i] = countryCode;
-        
-        // Add it to the scrollview
-        [_portraitScrollView addSubview:flag];
-    }
-    
     // Change the landscape flag and apply a border unless we're showing the globe
-    NSDictionary *info = countries[index];
+    NSDictionary *info = [DataManager sharedDataManager].countryData[selection];
     NSString *countryCode = info[@"code"];
     if ([countryCode isEqualToString:@"world"]) {
         // Set the globe image
@@ -158,25 +73,6 @@
     
     // Force a layout
     [self.view setNeedsLayout];
-}
-
-- (void)scrollViewTouched:(UIGestureRecognizer *)recognizer {
-    // Nothing to do if the scroll view is still animating
-    if (_portraitScrollView.layer.animationKeys.count)
-        return;
-    
-    // Nothing to do if the middle flag isn't the one that is
-    // currently shown, in case we haven't swapped the flags yet
-    if (_portraitScrollView.contentOffset.x != _portraitScrollView.frame.size.width)
-        return;
-    
-    // Check if the left arrow was touched
-    if (CGRectContainsPoint(_portraitArrowLeft.bounds, [recognizer locationInView:_portraitArrowLeft]))
-        [_portraitScrollView setContentOffset:CGPointMake(0, 0) animated:YES];
-    
-    // Check if the right arrow was touched
-    else if (CGRectContainsPoint(_portraitArrowRight.bounds, [recognizer locationInView:_portraitArrowRight]))
-        [_portraitScrollView setContentOffset:CGPointMake(_portraitScrollView .frame.size.width * 2, 0) animated:YES];
 }
 
 - (void)enableOrDisableViews:(BOOL)portrait {
@@ -246,44 +142,12 @@
         _portraitArrowLeft.center = CGPointMake(20 + _portraitArrowLeft.frame.size.width / 2, _portraitScrollView.center.y);
         _portraitArrowRight.center = CGPointMake(self.view.bounds.size.width -  20 - _portraitArrowRight.frame.size.width / 2, _portraitScrollView.center.y);
         
-        // Position the flags in the scroll view
-        for (int i = 0; i < 3; ++i) {
-            UIImageView *flag = _portraitFlags[i];
-            flag.center = CGPointMake(self.view.bounds.size.width * (i + 0.5), _portraitScrollView.frame.size.height / 2);
-        }
-        
         // Position the web view and its background
         CGRect frame = CGRectMake(20, 0, self.view.bounds.size.width - 40, 367);
         frame.origin.y = self.view.bounds.size.height - 20 - frame.size.height;
         _webView.frame = frame;
         _portraitWebViewBackground.frame = frame;
     }
-}
-
-- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
-    // This is called after the animation triggered
-    // by setting the content offset programatically
-    [self checkSelectedCountry];
-}
-
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    // This is called after the decelerating animation
-    // when the user releases the dragged scrollview
-    [self checkSelectedCountry];
-}
-
-- (void)checkSelectedCountry {
-    // Find the selected page, accounting for any rounding errors
-    int page = (_portraitScrollView.contentOffset.x + 0.1) / _portraitScrollView.frame.size.width;
-    assert(page == 0 || page == 1 || page == 2);
-    
-    // If it's the middle page, the selection hasn't changed
-    if (page == 1)
-        return;
-    
-    // Let others know about this selection
-    NSString *selection = _portraitCountryCodes[page];
-    [[NSNotificationCenter defaultCenter] postNotificationName:CountrySelectionNotification object:self userInfo:@{SelectedCountryKey : selection}];
 }
 
 @end
