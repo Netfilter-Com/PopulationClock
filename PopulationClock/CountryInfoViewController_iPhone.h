51c140c747c363dff4e5244c880436dade03d4e3
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-02-14 15:59:26 -0200

    A better layout for the country info screen

---
+++ PopulationClock/CountryInfoViewController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  CountryInfoViewController_iPhone.h
+//  PopulationClock
+//
+//  Created by Fernando Lemos on 14/02/13.
+//  Copyright (c) 2013 NetFilter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountryInfoViewController_iPhone : UIViewController
+
+@end
---
+++ PopulationClock/CountryInfoViewController_iPhone.m
@@ -0,0 +1,29 @@
+//
+//  CountryInfoViewController_iPhone.m
+//  PopulationClock
+//
+//  Created by Fernando Lemos on 14/02/13.
+//  Copyright (c) 2013 NetFilter. All rights reserved.
+//
+
+#import "CountryInfoViewController_iPhone.h"
+
+@interface CountryInfoViewController_iPhone ()
+
+@property (nonatomic, weak) IBOutlet UIView *leftPanel;
+@property (nonatomic, weak) IBOutlet UIView *rightPanel;
+@property (nonatomic, weak) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation CountryInfoViewController_iPhone
+
+- (void)viewWillLayoutSubviews
+{
+    CGFloat width = self.view.bounds.size.width / 2;
+    CGFloat height = self.view.bounds.size.height;
+    self.leftPanel.frame = CGRectMake(0, 0, width, height);
+    self.rightPanel.frame = CGRectMake(width, 0, width, height);
+}
+
+@end
