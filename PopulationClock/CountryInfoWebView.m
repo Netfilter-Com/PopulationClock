13da5ccd6eb15278489af43a794bc45ac7667214
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-05-15 17:08:23 -0300

    Move the info web view HTML reload to a bg thread

--- PopulationClock/CountryInfoWebView.m
+++ PopulationClock/CountryInfoWebView.m
@@ -13,10 +13,13 @@
 #import "StatsBuilder.h"
 
 @implementation CountryInfoWebView {
-    BOOL _didLayout;
-    UIInterfaceOrientation _interfaceOrientationForLayout;
     NSString *_selectedCountry;
     CALayer *_shadowLayer;
+
+    BOOL _hasContent;
+    BOOL _contentIsPortrait;
+
+    dispatch_queue_t _backgroundQueue;
 }
 
 + (Class)layerClass
@@ -25,6 +28,11 @@
 }
 
 - (void)awakeFromNib {
+    // Create the queue for background stuff
+    _backgroundQueue = dispatch_queue_create("br.com.netfilter.CountryInfoWebView", 0);
+    dispatch_queue_t lowPriorityQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    dispatch_set_target_queue(_backgroundQueue, lowPriorityQueue);
+
     // Disable bouncing
     self.scrollView.bounces = NO;
     
@@ -78,6 +86,11 @@
 - (void)dealloc {
     // We are no longer observers
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+
+    // Get rid of the queue
+    if (_backgroundQueue) {
+        dispatch_release(_backgroundQueue);
+    }
 }
 
 - (void)countrySelectionChanged:(NSNotification *)notification {
@@ -85,11 +98,97 @@
     _selectedCountry = notification.userInfo[SelectedCountryKey];
     
     // Force a layout
-    _didLayout = NO;
-    [self setNeedsLayout];
+    [self updateCountrySelection];
+}
+
+- (void)updateCountrySelection {
+    // Nothing to do until we have a selection
+    if (!_selectedCountry)
+        return;
+
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    BOOL isPortrait = UIInterfaceOrientationIsPortrait(orientation);
+
+    dispatch_async(_backgroundQueue, ^{
+        // Load the right template depending on the orientation
+        NSString *templateName = isPortrait ? @"info_template_portrait" : @"info_template_landscape";
+        NSString *path = [[NSBundle mainBundle] pathForResource:templateName ofType:@"html"];
+        NSString *template = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+
+        // Get the short paragraph about the country
+        NSString *description = [[NSBundle mainBundle] localizedStringForKey:_selectedCountry value:_selectedCountry table:@"Description"];
+
+        // Create the substitution dictionary
+        NSDictionary *substitutions;
+        if (isPortrait || UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+            NSDictionary *info = [DataManager sharedDataManager].countryData[_selectedCountry];
+            substitutions = @{
+                @"NAME" : info[@"name"],
+                @"DESCRIPTION" : description,
+                @"READ_MORE_LINK" : NSLocalizedString(@"[Read more]", @""),
+                @"STATS" : [[StatsBuilder new] statsStringForCountryCode:_selectedCountry]
+            };
+        }
+        else {
+            substitutions = @{ @"%%DESCRIPTION%%" : description };
+        }
+
+        char * const templateC = strdup([template UTF8String]);
+        char *templatePtr = templateC;
+
+        NSMutableString *result = [[NSMutableString alloc] initWithCapacity:template.length * 2];
+
+        // Perform the template substitutions in C because string replacement
+        // with NSStrings is way too slow (probably because of Unicode support)
+        for (;;) {
+            // Find the first delimiter
+            char *begin = strstr(templatePtr, "%%");
+
+            // If there's no first delimiter, just append the remainder
+            if (!begin) {
+                [result appendString:[NSString stringWithCString:templatePtr encoding:NSUTF8StringEncoding]];
+                break;
+            }
+
+            // Find the second delimiter
+            char *end = strstr(begin + 2, "%%");
+            NSAssert(end, @"Malformed template (missing second delimiter)");
+
+            // Zero-terminate the first char so from template to begin - 1
+            // so we have a zero-terminated C string
+            *begin = '\0';
+
+            // Append this to the result string
+            NSString *before = [NSString stringWithCString:templatePtr encoding:NSUTF8StringEncoding];
+            [result appendString:before];
+
+            // Zero-terminate the key and retrieve it
+            *end = '\0';
+            NSString *key = [NSString stringWithCString:begin + 2 encoding:NSUTF8StringEncoding];
+            NSString *replacement = substitutions[key];
+            NSAssert(replacement, @"Replacement not found for key: %@", key);
+            [result appendString:replacement];
+
+            // Advance the pointer to the template
+            templatePtr += end - templatePtr + 2;
+        }
+
+        free(templateC);
+        template = result;
+
+        // Load the HTML
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSURL *baseURL = [NSURL fileURLWithPath:[path stringByDeletingLastPathComponent] isDirectory:YES];
+            [self loadHTMLString:template baseURL:baseURL];
+        });
+    });
 }
 
 - (void)layoutSubviews {
+    // The first time the view is laid out, we don't have metrics
+    if (self.bounds.size.width == 0 || self.bounds.size.height == 0)
+        return;
+    
     // Set the shadow path
     CGFloat r = _shadowLayer.shadowRadius;
     CGMutablePathRef shadowPath = CGPathCreateMutable();
@@ -105,100 +204,21 @@
     CGPathAddLines(shadowPath, NULL, lines, sizeof(lines) / sizeof(CGPoint));
     _shadowLayer.shadowPath = shadowPath;
     CGPathRelease(shadowPath);
-    
-    // Nothing to do until we have a selection
-    if (!_selectedCountry)
-        return;
-    
-    // The first time the view is laid out, we don't have metrics
-    if (self.bounds.size.width == 0 || self.bounds.size.height == 0)
-        return;
-    
-    // Nothing to do if the interface orientation didn't change
+
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
     BOOL isPortrait = UIInterfaceOrientationIsPortrait(orientation);
-    if (_didLayout && isPortrait == UIInterfaceOrientationIsPortrait(_interfaceOrientationForLayout))
-        return;
-    
-    // Change the scroll bar behavior depending whether we're
-    // in portrait or landscape
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        self.scrollView.indicatorStyle = isPortrait ? UIScrollViewIndicatorStyleBlack : UIScrollViewIndicatorStyleWhite;
-    }
-    
-    // Load the right template depending on the orientation
-    NSString *templateName = isPortrait ? @"info_template_portrait" : @"info_template_landscape";
-    NSString *path = [[NSBundle mainBundle] pathForResource:templateName ofType:@"html"];
-    NSString *template = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
-    
-    // Get the short paragraph about the country
-    NSString *description = [[NSBundle mainBundle] localizedStringForKey:_selectedCountry value:_selectedCountry table:@"Description"];
-    
-    // Create the substitution dictionary
-    NSDictionary *substitutions;
-    if (isPortrait || UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-        NSDictionary *info = [DataManager sharedDataManager].countryData[_selectedCountry];
-        substitutions = @{
-            @"NAME" : info[@"name"],
-            @"DESCRIPTION" : description,
-            @"READ_MORE_LINK" : NSLocalizedString(@"[Read more]", @""),
-            @"STATS" : [[StatsBuilder new] statsStringForCountryCode:_selectedCountry]
-        };
-    }
-    else {
-        substitutions = @{ @"%%DESCRIPTION%%" : description };
-    }
 
-    char * const templateC = strdup([template UTF8String]);
-    char *templatePtr = templateC;
+    if (!_hasContent || isPortrait == _contentIsPortrait) {
+        // If the interface orientation changed, we have to force an update
+        [self updateCountrySelection];
+        _contentIsPortrait = isPortrait;
+        _hasContent = YES;
 
-    NSMutableString *result = [[NSMutableString alloc] initWithCapacity:template.length * 2];
-
-    // Perform the template substitutions in C because string replacement
-    // with NSStrings is way too slow (probably because of Unicode support)
-    for (;;) {
-        // Find the first delimiter
-        char *begin = strstr(templatePtr, "%%");
-
-        // If there's no first delimiter, just append the remainder
-        if (!begin) {
-            [result appendString:[NSString stringWithCString:templatePtr encoding:NSUTF8StringEncoding]];
-            break;
+        // Change the scroll view indicator style
+        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+            self.scrollView.indicatorStyle = isPortrait ? UIScrollViewIndicatorStyleBlack : UIScrollViewIndicatorStyleWhite;
         }
-
-        // Find the second delimiter
-        char *end = strstr(begin + 2, "%%");
-        NSAssert(end, @"Malformed template (missing second delimiter)");
-
-        // Zero-terminate the first char so from template to begin - 1
-        // so we have a zero-terminated C string
-        *begin = '\0';
-
-        // Append this to the result string
-        NSString *before = [NSString stringWithCString:templatePtr encoding:NSUTF8StringEncoding];
-        [result appendString:before];
-
-        // Zero-terminate the key and retrieve it
-        *end = '\0';
-        NSString *key = [NSString stringWithCString:begin + 2 encoding:NSUTF8StringEncoding];
-        NSString *replacement = substitutions[key];
-        NSAssert(replacement, @"Replacement not found for key: %@", key);
-        [result appendString:replacement];
-
-        // Advance the pointer to the template
-        templatePtr += end - templatePtr + 2;
     }
-
-    free(templateC);
-    template = result;
-    
-    // Load the HTML
-    NSURL *baseURL = [NSURL fileURLWithPath:[path stringByDeletingLastPathComponent] isDirectory:YES];
-    [self loadHTMLString:template baseURL:baseURL];
-    
-    // Save the new orientation
-    _interfaceOrientationForLayout = orientation;
-    _didLayout = YES;
 }
 
 @end
