74b2bc3f2ca0d26b05683369f506772151627adc
João Bernardo Oliveira <jboliveira@netfilter.com>
2016-02-23 12:11:09 -0300

    Fixed styling and initial python 3 support

--- Scripts/description/scrapCIA.py
+++ Scripts/description/scrapCIA.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import urllib
 import constants
 from bs4 import BeautifulSoup
@@ -8,29 +9,33 @@ url = "https://www.cia.gov/library/publications/the-world-factbook/geos/"
 
 country_table = [i.rstrip().split(";") for i in open("ciaCountryCode.txt").readlines()]
 
-def escape ( text, characters ):
+
+def escape(text, characters):
     for character in characters:
-        text = text.replace( character, '\\' + character )
+        text = text.replace(character, '\\' + character)
     return text
 
+
 def quot(st):
-        return '"'+ st.replace("\n","\\n").replace("\r","\\r") + '"'
+    return '"' + st.replace("\n", "\\n").replace("\r", "\\r") + '"'
+
 
 def main():
-	for i in COUNTRY_CODES:
-		country_code = "error"
-		for j in country_table:
-			if j[2]==i.upper():
-				country_code = j[1].lower()
-				break
-		if country_code == "error":
-			continue
-		data = urllib.urlopen(url + country_code + ".html").read()
-		soup = BeautifulSoup(data)
-		desc = soup.find("div", { "class" : "category_data" })
-		try:
-			print "%s = %s;"%(quot(i),quot(escape(desc.text,"\"")))
-		except:
-		    print i
+    for i in COUNTRY_CODES:
+        country_code = "error"
+        for j in country_table:
+            if j[2] == i.upper():
+                country_code = j[1].lower()
+                break
+        if country_code == "error":
+            continue
+        data = urllib.urlopen(url + country_code + ".html").read()
+        soup = BeautifulSoup(data, 'html.parser')
+        desc = soup.find("div", {"class": "category_data"})
+        try:
+            print("%s = %s;" % (quot(i), quot(escape(desc.text, "\""))))
+        except Exception:
+            print(i)
+
 
 main()
